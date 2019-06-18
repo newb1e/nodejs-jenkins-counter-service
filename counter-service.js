e3300ce2035bcdf08ab7323770aee4dd00578d6d
newb1e <ovst91@gmail.com>
2019-06-18 16:14:03 +0300

    write counter service and create repo

---
+++ counter-service.js
@@ -0,0 +1,22 @@
+var http = require('http');
+
+var userCount = 0;
+
+var server = http.createServer(function (req, res) {
+
+    if (req.method === 'POST')
+    {
+        userCount++;
+        res.end();
+    }
+    else if (req.method === 'GET'){
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.write('We have had ' + userCount + ' POST requests!\n');
+        res.end();
+    }
+
+});
+
+server.listen(1080);
+
+console.log('server running...');
